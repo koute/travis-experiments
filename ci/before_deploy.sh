f7918b847005ab470ec40eff32624cec33f24316
Jan Bujak <j@exia.io>
2018-02-03 16:00:02 +0100

    Update

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 set -euo pipefail
-IFS=$'\n\t'
 
 if [ -z "${DEPLOY_TARGETS-}" ]; then
     exit 1
@@ -10,6 +9,7 @@ fi
 echo "Building artifacts for deployment..."
 
 for TARGET in $DEPLOY_TARGETS; do
+    echo "Target: $TARGET"
     rustup target add $TARGET
     cargo build --release --target=$TARGET
     cat target/$TARGET/release/$PROJECT_NAME | gzip > $PROJECT_NAME-$TARGET.gz
