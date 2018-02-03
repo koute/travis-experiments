460ed9c0e0157b57530e48e1ba82baaf87816d90
Jan Bujak <j@exia.io>
2018-02-03 15:50:22 +0100

    Update

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -3,6 +3,12 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+if [ -z "${DEPLOY_TARGETS-}" ]; then
+    exit 1
+fi
+
+echo "Building artifacts for deployment..."
+
 for TARGET in $DEPLOY_TARGETS do
     rustup target add $TARGET
     cargo build --release --target=$TARGET
