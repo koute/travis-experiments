34a29a4a4953606f1d53d33b0394d273b6012854
Jan Bujak <j@exia.io>
2018-02-03 15:21:40 +0100

    Update

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -3,10 +3,6 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-if [ ! "$DEPLOY" = "1" ]; then
-    exit 0
-fi
-
 for TARGET in $DEPLOY_TARGETS do
     rustup target add $TARGET
     cargo build --release --target=$TARGET
