dd37496e3bc3b2b334fc94cb302a757f4e51c54f
Jan Bujak <j@exia.io>
2018-02-03 14:48:35 +0100

    Update

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -7,7 +7,7 @@ if [ ! "$DEPLOY" = "1" ]; then
     exit 0
 fi
 
-for TARGET in "${DEPLOY_TARGET[@]}"; do
+for TARGET in $DEPLOY_TARGETS do
     rustup target add $TARGET
     cargo build --release --target=$TARGET
     cat target/$TARGET/release/$PROJECT_NAME | gzip > $PROJECT_NAME-$TARGET.gz
